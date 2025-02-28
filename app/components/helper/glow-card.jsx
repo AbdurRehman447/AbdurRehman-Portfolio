7c7e5bc7db042fcb200062f9fcbf5ce175c64e11
AbdurRehman <167701754+AbdurRehman447@users.noreply.github.com>
2025-02-28 21:39:11 +0500

    glow-card.jsx

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -1,10 +1,10 @@
-"use client";
+"use client";  // This forces Next.js to run this component only on the client
 
 import { useEffect } from "react";
 
 const GlowCard = ({ children, identifier }) => {
   useEffect(() => {
-    if (typeof window === "undefined") return; // Ensure this runs only on the client
+    if (typeof window === "undefined") return; // Prevents SSR issues
 
     const CONTAINER = document.querySelector(`.glow-container-${identifier}`);
     const CARDS = document.querySelectorAll(`.glow-card-${identifier}`);
