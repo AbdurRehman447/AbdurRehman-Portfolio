8b485383a82853209d858897a8dbecceb8675296
AbdurRehman <167701754+AbdurRehman447@users.noreply.github.com>
2025-03-09 23:17:24 +0500

    page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 "use client"; // Ensure this file runs only in the browser
 
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+
+// Dynamically import components that might use `document`
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
 
 export default function Home() {
   const [blogs, setBlogs] = useState([]);
