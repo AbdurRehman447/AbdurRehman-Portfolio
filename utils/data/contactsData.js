798b03fe1441bdcd56626bf418c47be7b972db18
AbdurRehman <167701754+AbdurRehman447@users.noreply.github.com>
2025-03-10 10:06:04 +0500

    contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
-}
+    email: 'info@abdurrehman.online',
+    phone: '+92 313 1444779',
+    address: 'Lahore, Pakistan',
+    github: 'https://github.com/AbdurRehman447',
+    facebook: 'https://www.facebook.com/profile.php?id=100091908776684',
+    linkedIn: 'https://www.linkedin.com/in/mani-baig/',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: ""
+};
